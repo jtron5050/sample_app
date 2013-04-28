92b1e76273ad5caf57a3a18db06b4f01d948dd07
Joshua Thomas <jtron5050@me.com>
2013-04-28 17:08:35 -0400

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -29,6 +29,7 @@ gem 'jquery-rails'
 
 group :test do
   gem 'capybara', '1.1.2'
+  gem 'factory_girl_rails', '4.2.1'
 end
 
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name 'Joshua Thomas'
+    email 'jtron5050@me.com'
+    password 'foobar'
+    password_confirmation 'foobar'
+  end
+end
