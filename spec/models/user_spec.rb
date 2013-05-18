3fae718179427b89e90a65e364a8f2eecae28359
Joshua Thomas <jtron5050@me.com>
2013-05-18 10:54:23 -0400

    Add microposts model and controller

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,8 @@ describe User do
   it { should respond_to :remember_token }
   it { should respond_to :admin }
 
+  it { should respond_to :microposts }
+
   it { should be_valid }
   it { should_not be_admin }
 
@@ -105,7 +107,7 @@ describe User do
     it { should_not be_valid }
   end
 
-  describe 'return value of autenticate method' do
+  describe 'return value of authenticate method' do
     before { @user.save }
     let(:found_user) { User.find_by_email(@user.email) }
 
@@ -124,6 +126,27 @@ describe User do
     before { @user.save }
 
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe 'micropost associations' do
+    before { @user.save }
+    let!(:older_micropost) { FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago) }
+    let!(:newer_micropost) { FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago) }
+
+    it 'should have the right microposts in the right order' do
+      @user.microposts.should == [newer_micropost, older_micropost]
+
+    end
+
+    it 'should destroy associated microposts' do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
   end
 
 end
