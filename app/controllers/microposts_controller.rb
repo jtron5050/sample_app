8236d065169d177dcd2a7250d86ea0eaae7515e4
Joshua Thomas <jtron5050@me.com>
2013-05-25 08:27:42 -0400

    Finish user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,12 +7,16 @@ class MicropostsController < ApplicationController
       flash[:success] = 'Micropost created!'
       redirect_to root_url
     else
+      @feed_items = []
       render 'static_pages/home'
     end
   end
 
   def destroy
-
+    @micropost = current_user.microposts.find_by_id(params[:id])
+    redirect_to root_url if @micropost.nil?
+    @micropost.destroy
+    redirect_to root_url
   end
 
 end
